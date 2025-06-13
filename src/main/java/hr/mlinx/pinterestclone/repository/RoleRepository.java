0ee7f3e1ba65c28a167fbc3e9656f1ea3b86871e
Vinicius Cruz <viniciusfernando@gmail.com>
2025-06-13 16:24:37 -0300

    Update RoleRepository.java

--- src/main/java/hr/mlinx/pinterestclone/repository/RoleRepository.java
+++ src/main/java/hr/mlinx/pinterestclone/repository/RoleRepository.java
@@ -3,9 +3,13 @@ package hr.mlinx.pinterestclone.repository;
 import hr.mlinx.pinterestclone.model.Role;
 import hr.mlinx.pinterestclone.model.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(RoleName name);
+
+    boolean existsByName(RoleName name); // ← ADICIONE ESTA LINHA
 }
