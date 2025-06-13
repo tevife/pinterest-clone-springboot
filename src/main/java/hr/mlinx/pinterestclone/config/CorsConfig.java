3e8efd6ef1d0f4f8d21c880df3262cfedd352fca
Vinicius Cruz <viniciusfernando@gmail.com>
2025-06-13 15:39:30 -0300

    Update CorsConfig.java

--- src/main/java/hr/mlinx/pinterestclone/config/CorsConfig.java
+++ src/main/java/hr/mlinx/pinterestclone/config/CorsConfig.java
@@ -1,26 +1,24 @@
 package hr.mlinx.pinterestclone.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
-    
+
     @Bean
     public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
+
         return new CorsFilter(source);
-    // <- esqueceu de fechar esse método aqui
+    }
 }
-// <- esqueceu de fechar a classe aqui
